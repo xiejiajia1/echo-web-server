ce6f7645de34fd47fb7072cc9818ad3134d8d7fd
caijing <caijing.cai@alibaba-inc.com>
2023-08-25 17:39:07 +0800

    update log to echo web server

--- main.go
+++ main.go
@@ -2,23 +2,22 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/gorilla/mux"
 	logs "github.com/sirupsen/logrus"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		logs.Infof("Hello Echo Server")
-		json.NewEncoder(w).Encode("Hello Echo Server")
+		logs.Infof("Hello Echo Web Server")
+		json.NewEncoder(w).Encode("Hello Echo Web Server")
 	})
 
 	router.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
-		logs.Infof("Hello Echo Server v2.0 @oversea")
-		json.NewEncoder(w).Encode("Hello Echo Server v2.0 @oversea")
+		logs.Infof("Hello Echo Web Server v2.0 @oversea")
+		json.NewEncoder(w).Encode("Hello Echo Web Server v2.0 @oversea")
 	})
 
 	http.ListenAndServe(":8080", router)
